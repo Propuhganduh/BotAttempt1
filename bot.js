1fd88de6adcf33b63187456541bde5827f61324d
Propuhganduh <JosephLG27@outlook.com>
2015-11-19 23:07:50 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,7 +51,7 @@ function respond() {
     this.res.writeHead(200);
     var req = request.text.substring(5,request.text.length);
     var rep = req.replace(/ /,"+");
-    postMessage("http://daddyleagues.com/jvl/players?name="+rep+"&position=all&team=all");
+    postMessage("http://daddyleagues.com/FITG/players?name="+rep+"&position=all&team=all");
     this.res.end();
   } 
 
