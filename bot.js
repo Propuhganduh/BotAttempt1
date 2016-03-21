e881156a3d31b0a4442e064eb17b8b591c13ec45
Propuhganduh <JosephLG27@outlook.com>
2016-03-20 23:56:29 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,70 +19,70 @@ function respond() {
   } 
   else if(request.text && botRegexDL.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://i.imgur.com/yNBtNiy.gif");
+    postMessage("http://daddyleagues.com/F1TG/team/"+request.text.substring(5,8)+"/depthchart");
     this.res.end();
   } 
   else if(request.text && botRegexSalt.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("¯\\_(ツ)_/¯");
+    postMessage("http://i.imgur.com/q3ohLtD.gif");
     this.res.end();
   } 
   else if(request.text && botRegexAd.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("¯\\_(ツ)_/¯");
+    postMessage("http://www.kno.com/images/store/knoadvance/overview.png");
     this.res.end();
   }
   else if(request.text && botRegexYub.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("¯\\_(ツ)_/¯");
+    postMessage("http://www.quickmeme.com/img/9f/9f3720469d1ce6c2d20130ed0750935a394df80ffcddec6d16e091d95efea854.jpg");
     this.res.end();
   } 
   else if(request.text && botRegexBan.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("¯\\_(ツ)_/¯");
+    postMessage("http://www.returnofkings.com/wp-content/uploads/2014/03/ban.jpg");
     this.res.end();
   } 
   else if(request.text && botRegexSC.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("¯\\_(ツ)_/¯");
+    postMessage("http://daddyleagues.com/F1TG/team/"+request.text.substring(5,8)+"/schedule");
     this.res.end();
   }
   else if(request.text && botRegexP.test(request.text)) {
     this.res.writeHead(200);
     var req = request.text.substring(5,request.text.length);
     var rep = req.replace(/ /,"+");
-    postMessage("The manual life is a good one");
+    postMessage("http://daddyleagues.com/F1TG/players?name="+rep+"&position=all&team=all");
     this.res.end();
   } 
 
   else if(request.text && botRegexTw.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://i.imgur.com/yNBtNiy.gif);
+    postMessage("http://www.twitch.tv/"+request.text.substring(8,request.text.length));
     this.res.end();
   } 
   else if(request.text && botRegexSb.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("¯\\_(ツ)_/¯");
+    postMessage("http://www.reddit.com/r/fitg");
     this.res.end();
   }
   else if(request.text && botRegexCh.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("¯\\_(ツ)_/¯");
+    postMessage("http://content.science20.com/files/images/not%20GMO%20vegan%20cheese%20but%20could%20you%20tell%20anyway.jpg");
     this.res.end();
   } 
   else if(request.text && botRegexCMN.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("¯\\_(ツ)_/¯");
+    postMessage("http://i.groupme.com/236x231.png.9f975e73eab5431094746b0b30f0945e.large");
     this.res.end();
   } 
   else if(request.text && botRegexh2h.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("¯\\_(ツ)_/¯");
+    postMessage("http://i.groupme.com/1350x904.png.f5047b2f1c154316abd1b2af6ddd3bdf.large");
     this.res.end();
   } 
   else if(request.text && botRegexDef.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("¯\\_(ツ)_/¯");
+    postMessage("http://crimsoncast.com/wp-content/uploads/2010/12/D-Fence.jpg");
     this.res.end();
   } 
   else {
